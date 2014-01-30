1c53b8ad0e6d26f3c90ac3108a45f8c8b7e30494
majesticpi <ip5141.3@gmail.com>
2014-01-30 21:39:09 +0100

    Now with upper and lower end
    Default values for calculation on index.html not working

--- RogueTrader.py
+++ RogueTrader.py
@@ -13,7 +13,13 @@ class Result(ndb.Model):
     average_mc = ndb.FloatProperty()
     average_la = ndb.FloatProperty()
     average_marc = ndb.FloatProperty()
+    ballistic = ndb.IntegerProperty()
     armour = ndb.IntegerProperty()
+    dmg_type = ndb.StringProperty()
+    
+    @classmethod
+    def dmg_query_bs(cls, BS):
+        return cls.query(cls.ballistic == BS).order(cls.armour)
     
 class Weapon(ndb.Model):
     number = ndb.IntegerProperty()
@@ -24,8 +30,7 @@ class Weapon(ndb.Model):
     
     @classmethod
     def query_weapon(cls, choice):
-        return cls.query(cls.number == choice).fetch()
-    
+        return cls.query(cls.number == choice).fetch()        
     
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -84,11 +89,13 @@ class MainPage(webapp2.RequestHandler):
 class CalcPage(webapp2.RequestHandler):
 
     def post(self):
-        BS = int(self.request.get('BS'))
+        BSu = int(self.request.get('BSu'))
+        BSl = int(self.request.get('BSl'))
+        interval = int(self.request.get('Interval'))
+        if (interval < 1): interval = 1
         choice = int(self.request.get('choice'))
         chosen_test = Weapon.query(Weapon.number == choice).fetch(1)
         
-                
         for property in chosen_test:
             name = property.name
             type = property.type
@@ -99,33 +106,38 @@ class CalcPage(webapp2.RequestHandler):
         if (type == 'LA'): chosen_type = 'Lance'
         weapon_choice = str(name)+ '-' + chosen_type + '\tStrength: ' + str(strength) + '\tDamage: 1d10+' + str(dmg_bonus)
         
-        
-        armour = 12
+        bs_list = []
         dmg_bonus = int(dmg_bonus)
-        while (armour < 25):
-            count = 0.0
-            total_dmg_mc = 0.0
-            total_dmg_la = 0.0
-            total_dmg_marc = 0.0
-            while (count < 10000):
-                count += 1
-                deg = ToHit(BS, strength)
+        bs_count = 0
+        while (BSl < (BSu + 1)):
+            bs_count += 1
+            bs_list.append(BSl)
+            armour = 12
+            while (armour < 25):
+                count = 0.0
+                total_dmg_mc = 0.0
+                total_dmg_la = 0.0
+                total_dmg_marc = 0.0
+                while (count < 10000):
+                    count += 1
+                    deg = ToHit(BSl, strength)
+                    if (type == 'MC'):
+                        total_dmg_mc += DMG_mc(dmg_bonus, deg, armour)
+                        total_dmg_marc += DMG_marc(dmg_bonus, deg, armour)
+                    if (type == 'LA'):
+                        total_dmg_la += DMG_la(dmg_bonus, deg, armour)
                 if (type == 'MC'):
-                    total_dmg_mc += DMG_mc(dmg_bonus, deg, armour)
-                    total_dmg_marc += DMG_marc(dmg_bonus, deg, armour)
+                    result = Result(average_mc = total_dmg_mc/count, average_marc = total_dmg_marc/count, average_la = total_dmg_la/count, armour = armour, ballistic = BSl, dmg_type = type)
                 if (type == 'LA'):
-                    total_dmg_la += DMG_la(dmg_bonus, deg, armour)
-            if (type == 'MC'):
-                result = Result(average_mc = total_dmg_mc/count, average_marc = total_dmg_marc/count, average_la = total_dmg_la/count, armour = armour)
-            if (type == 'LA'):
-                result = Result(average_la = total_dmg_la/count, armour = armour)
-            result.put()          
-            armour += 1
+                    result = Result(average_la = total_dmg_la/count, armour = armour, ballistic = BSl, dmg_type = type)
+                result.put()          
+                armour += 1
+            BSl += interval
         
         dmg_query = result.query(ndb.AND(Result.armour > 11, ndb.AND(Result.armour < 21))).order(Result.armour)
         
         template_values = {
-            'BS': BS,
+            'bs_list': bs_list,
             'type': type,
             'dmg_query': dmg_query,
             'weapon_choice': weapon_choice,
@@ -134,20 +146,11 @@ class CalcPage(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('calc.html')
         self.response.write(template.render(template_values))
         
+        ndb.delete_multi(Result.query().fetch(keys_only=True))
+        
 class ChangePage(webapp2.RequestHandler):
 
     def post(self):
-        
-        # ndb.delete_multi(Weapon.query().fetch(keys_only=True))
-        # path_to_Cannon_file = "./Weapons.txt"
-        # cannons_csv = csv.DictReader(open(path_to_Cannon_file, "r"), delimiter=";")
-        # cannons = {}
-        # item_counter = 1
-        # for cannon in cannons_csv:
-            # cannons[item_counter] = cannon
-            # weapon = Weapon(number = item_counter, type = cannon['Type'], name = cannon['Name'], strength = int(cannon['Strength']), dmg_bonus = int(cannon['DamageBonus']))
-            # weapon.put()
-            # item_counter += 1
         
         testa = Weapon.query().fetch(keys_only=True)
         weapons = Weapon.query().order(Weapon.number)
@@ -165,7 +168,7 @@ class Reset(webapp2.RequestHandler):
 
     def post(self):
         ndb.delete_multi(Weapon.query().fetch(keys_only=True))
-        path_to_Cannon_file = "./Weapons1.txt"
+        path_to_Cannon_file = "./Weapons.txt"
         cannons_csv = csv.DictReader(open(path_to_Cannon_file, "r"), delimiter=";")
         cannons = {}
         item_counter = 1
